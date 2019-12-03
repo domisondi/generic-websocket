bed208f190212a2f67ca8f90a1253016afbace3f
Dominic Sonderegger <dominic.sonderegger@hispeed.ch>
2019-12-03 23:13:01 +0100

    use host as url

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ module.exports = {
 	allowed_broadcast_ips: ["127.0.0.1", "::1", "::ffff:127.0.0.1", "192.168.123.5", "::ffff:192.168.123.5"],
 	auth_cookie_full: false,
 	auth_cookie: 'wordpress_logged_in_',
-	auth_url: 'https://tramp.hidden.tools/wp-content/plugins/tramp/socket/validate_cookie.php'
+	auth_url: '/wp-content/plugins/tramp/socket/validate_cookie.php'
 };
