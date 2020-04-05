d407b3307e2403a4ed2ebc869b2a5d88a7a643fc
Dominic Sonderegger <dominic.sonderegger@hispeed.ch>
2020-04-05 18:47:57 +0200

    compare as strings

--- User.js
+++ User.js
@@ -1,12 +1,14 @@
+const _ = require('underscore');
+
 var User = class User {
 	constructor(id, blogs) {
 		this.id = id;
-		this.blogs = blogs;
+		this.blogs = _.map(blogs, b => b + ''); // make sure it is stored as strings
 	}
 	
 	includesBlog(blog){
 		var blogs = this.blogs || [];
-		return blogs.includes(blog);
+		return blogs.includes(blog + ''); // compare as strings
 	}
 };
 
