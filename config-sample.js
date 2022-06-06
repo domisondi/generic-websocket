411a736fbf786fdbe3ec5dd59dcedba73db3385f
Dominic <dominic@hidden.tools>
2022-06-06 20:59:16 +0200

    default ping timeout for the server

--- config-sample.js
+++ config-sample.js
@@ -1,7 +1,10 @@
 module.exports = {
 	port: 8080,
 	is_behind_proxy: true,
-	socket_io: {path: '/websocket'},
+	socket_io: {
+		path: '/websocket',
+		pingTimeout: 60000
+	},
 	emit_event: 'relay',
 	broadcast_route: '/websocket',
 	allowed_broadcast_ips: ["127.0.0.1", "::1", "::ffff:127.0.0.1", "192.168.123.5", "::ffff:192.168.123.5"],
